a4f3f307a15e436f81827e1972033f347b807cd1
Malu Martins <128868371+malumartinss@users.noreply.github.com>
2023-05-19 12:50:04 -0300

    js

--- interações.js
+++ interações.js
@@ -4,9 +4,9 @@ function verificarLogin() {
     const linkEntrar = document.getElementById('linkEntrar') 
     const alert = document.getElementById('alert')
 
-    if (usuario == 'usuario' && senha == 'senha') {
+    if (usuario === 'usuario' && senha === 'senha') {
         linkEntrar.setAttribute('href', 'index.html')
-    } else if (usuario != 'usuario' || senha != 'senha') {
+    } else if (usuario !== 'usuario' || senha !== 'senha') {
         alert.classList.remove('invisible')
         alert.classList.add('visible')
     }
